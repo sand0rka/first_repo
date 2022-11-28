811eb0ae7d9d8261ba6e54db4cb9a1ee4e484093
sand0rka <syrotysss@gmail.com>
2022-11-28 11:24:22 +0200

    adding first commit to repo

---
+++ lab1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+	float x,y,res;
+
+	x = 0.042;
+	y = 3.03;
+	res = 13*pow(x,4) - log10 (y+x) + tan(sqrt(y));
+
+	printf("%f \n", res);
+	return 0;
+}
